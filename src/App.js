d79ccd203d34600f1798cb9da301ff77beb6d840
napoleQuang <minhquang121203@gmail.com>
2023-08-20 16:49:57 +0700

    header table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import Header from "./component/Header";
+// import Table from "./component/Table";
+
+// import Container from 'react-bootstrap/Container';
 
 function App() {
-  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Qunafg</h1>
+      {/* <Header />
+      <Container>
+        <Table />
+      </Container> */}
     </div>
   );
 }
